7b02d2d40e49d2abed5a91169fafbee5b9029c11
jasperP <53663744+jasperpwj@users.noreply.github.com>
2020-02-26 23:11:34 -0500

    new function file for hw05 mock testing

--- HW04_Weijie_Pan.py
+++ HW04_Weijie_Pan.py
@@ -1,36 +1,41 @@
 import requests
-import json
-import pprint
 
 
 def read_github_repo(id):
     url = 'https://api.github.com/users/' + id + '/repos'
     content = requests.get(url)
     repo = content.json()
-    print(type(repo))
 
     if isinstance(repo, dict):  # verify if the user id exist
-        return f"Cannot find the repo with the id: {id}"
+        print(f"Cannot find the repo with the id: {id}")
+        exit()
 
     elif 'name' not in repo[0].keys():  # verify if the user has any repository
-        return f"There is zero repository in the account: {id}"
-    else:
-        repo_result = {}
-        for name in repo:
-            url_commit = 'https://api.github.com/repos/' + id + '/' + name['name'] + '/commits'
-            result = requests.get(url_commit)
-            commit = result.json()
-            number = len(commit)
-            repo_result[name['name']] = number
+        print(f"There is zero repository in the account: {id}")
+        exit()
 
-        for key, value in repo_result.items():
-            print('Repo:', key, 'Number of commits: ', value)
-        return repo_result
+    else:
+        return repo
+
+
+def read_github_commit(id):
+    repo_result = {}
+    repo = read_github_repo(id)
+    for name in repo:
+        url_commit = 'https://api.github.com/repos/' + id + '/' + name['name'] + '/commits'
+        result = requests.get(url_commit)
+        commit = result.json()
+        number = len(commit)
+        repo_result[name['name']] = number
+
+    for key, value in repo_result.items():
+        print('Repo:', key, 'Number of commits: ', value)
+    return repo_result
 
 
 def main():
     name = input("Please input the github id: ")
-    read_github_repo(name)
+    read_github_commit(name)
 
 
 if __name__ == '__main__':
